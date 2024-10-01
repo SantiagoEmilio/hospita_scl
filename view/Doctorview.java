8491409c49e2fa468f338a289c96d26a71e8b26d
Santiago Emilio López Pocón <159474510+SantiagoEmilio@users.noreply.github.com>
2024-09-30 18:37:56 -0600

    Add files via upload
    creacion de doctor view

---
+++ view/Doctorview.java
@@ -0,0 +1,53 @@
+package view;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class Doctorview  extends JFrame{
+    private JLabel nombreDoctorLabel;
+    private JLabel especialidadLabel;
+
+    private int[] pantalla = {1300, 800};
+
+    public Doctorview(String nombreDoctor, String especialidad) {
+        // Configuración básica del JFrame
+        setTitle("Hospital Santa Catalina - Perfil del doctor");
+        setSize(pantalla[0], pantalla[1]);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setLayout(new BorderLayout());
+
+        // Panel Header
+        JPanel headerPanel = new JPanel();
+        headerPanel.setPreferredSize(new Dimension(1300, 60));
+        headerPanel.setBackground(Color.DARK_GRAY);
+        headerPanel.setLayout(new BorderLayout());
+
+        // JLabel para el nombre del hospital
+        JLabel hospitalLabel = new JLabel("Hospital Santa Catalina");
+        hospitalLabel.setForeground(Color.WHITE);
+        hospitalLabel.setFont(new Font("Arial", Font.BOLD, 20));
+        hospitalLabel.setHorizontalAlignment(SwingConstants.LEFT);
+        headerPanel.add(hospitalLabel, BorderLayout.WEST);
+
+        // Información del doctor
+        JPanel doctorInfoPanel = new JPanel();
+        doctorInfoPanel.setLayout(new GridLayout(2, 1));
+        doctorInfoPanel.setBackground(Color.DARK_GRAY);
+
+        // Etiquetas para el nombre y especialidad del doctor
+        nombreDoctorLabel = new JLabel("Nombre del doctor: " + nombreDoctor);
+        nombreDoctorLabel.setForeground(Color.WHITE);
+        especialidadLabel = new JLabel("Especialidad: " + especialidad);
+        especialidadLabel.setForeground(Color.WHITE);
+
+        ;
+
+        doctorInfoPanel.add(nombreDoctorLabel);
+        doctorInfoPanel.add(especialidadLabel);
+        headerPanel.add(doctorInfoPanel, BorderLayout.EAST);
+        add(headerPanel, BorderLayout.NORTH);
+
+    }
+}
